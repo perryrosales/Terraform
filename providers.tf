41c347fc4d447cb7ede62d750e68b804c4fe7e04
FMG\fmgpr2 <perry.rosales@fmg.co.nz>
2021-07-15 14:05:56 +1200

    add files

---
+++ providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.64.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+  subscription_id = "eb71c486-b02c-4a0c-97ed-1526d49e914f"
+}
