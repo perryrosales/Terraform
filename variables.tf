327eb4cb68cbf4472b37193882153621bc23181e
FMG\fmgpr2 <perry.rosales@fmg.co.nz>
2021-07-15 15:58:15 +1200

    Add nsg

--- variables.tf
+++ variables.tf
@@ -11,3 +11,22 @@ variable "location" {
 variable "tags" {
   type = map(any)
 }
+
+variable "vnetname" {
+
+    type = string
+  
+}
+
+variable "nsgName" {
+    type = string
+   
+}
+
+variable "subnet1Name" {
+    type = string
+}
+
+variable "subnet2Name" {
+    type = string
+}
